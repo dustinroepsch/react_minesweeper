8b6485070b42f5c9258f60e1128ab69d360742e7
Dustin Ryan-Roepsch <duryanro@microsoft.com>
2024-09-06 11:33:40 -0700

    use prettier defaults

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,53 +1,51 @@
-import { makeStyles } from '@griffel/react'
-import Minesweeper from '../minesweeper'
-import CellUI from './Cell'
+import { makeStyles } from "@griffel/react";
+import Minesweeper from "../minesweeper";
+import CellUI from "./Cell";
 export default function Board(props: {
-    minesweeper: Minesweeper
-    setMinesweeper: (
-        minesweeper: Minesweeper | ((minesweeper: Minesweeper) => Minesweeper)
-    ) => void
+  minesweeper: Minesweeper;
+  setMinesweeper: (
+    minesweeper: Minesweeper | ((minesweeper: Minesweeper) => Minesweeper),
+  ) => void;
 }): JSX.Element {
-    const classes = useStyles()
+  const classes = useStyles();
 
-    if (props.minesweeper.gamestate === 'won') {
-        return <div>You won!</div>
-    }
+  if (props.minesweeper.gamestate === "won") {
+    return <div>You won!</div>;
+  }
 
-    if (props.minesweeper.gamestate === 'lost') {
-        return <div>You lost!</div>
-    }
+  if (props.minesweeper.gamestate === "lost") {
+    return <div>You lost!</div>;
+  }
 
-    return (
-        <div className={classes.board}>
-            {props.minesweeper.cells.flat().map((cell, index) => (
-                <CellUI
-                    key={index}
-                    cell={cell}
-                    revealCell={() => {
-                        const { row, col } =
-                            props.minesweeper.indexToCoordinate(index)
-                        props.setMinesweeper((minesweeper) =>
-                            minesweeper.clickCell(row, col)
-                        )
-                    }}
-                    toggleFlagged={() => {
-                        const { row, col } =
-                            props.minesweeper.indexToCoordinate(index)
+  return (
+    <div className={classes.board}>
+      {props.minesweeper.cells.flat().map((cell, index) => (
+        <CellUI
+          key={index}
+          cell={cell}
+          revealCell={() => {
+            const { row, col } = props.minesweeper.indexToCoordinate(index);
+            props.setMinesweeper((minesweeper) =>
+              minesweeper.clickCell(row, col),
+            );
+          }}
+          toggleFlagged={() => {
+            const { row, col } = props.minesweeper.indexToCoordinate(index);
 
-                        props.setMinesweeper((minesweeper) =>
-                            minesweeper.toggleFlagged(row, col)
-                        )
-                    }}
-                />
-            ))}
-        </div>
-    )
+            props.setMinesweeper((minesweeper) =>
+              minesweeper.toggleFlagged(row, col),
+            );
+          }}
+        />
+      ))}
+    </div>
+  );
 }
 
 const useStyles = makeStyles({
-    board: {
-        display: 'grid',
-        gridTemplateColumns: 'repeat(10, min-content)',
-        gap: '1px',
-    },
-})
+  board: {
+    display: "grid",
+    gridTemplateColumns: "repeat(10, min-content)",
+    gap: "1px",
+  },
+});
--- src/minesweeper.ts
+++ src/minesweeper.ts
@@ -1,142 +1,136 @@
-import { immerable, produce } from 'immer'
+import { immerable, produce } from "immer";
 
 export interface Cell {
-    isMine: boolean
-    state: 'hidden' | 'revealed' | 'flagged'
-    numberOfNeighbourMines: number
+  isMine: boolean;
+  state: "hidden" | "revealed" | "flagged";
+  numberOfNeighbourMines: number;
 }
 
 interface Coordinate {
-    row: number
-    col: number
+  row: number;
+  col: number;
 }
 
 export default class Minesweeper {
-    [immerable] = true
+  [immerable] = true;
 
-    public readonly cells: Cell[][]
-    public gamestate: 'won' | 'lost' | 'playing' = 'playing'
+  public readonly cells: Cell[][];
+  public gamestate: "won" | "lost" | "playing" = "playing";
 
-    constructor(
-        public readonly rows: number,
-        public readonly cols: number,
-        public readonly mines: number
-    ) {
-        this.cells = Array.from({ length: rows }, () =>
-            Array.from({ length: cols }, () => ({
-                isMine: false,
-                state: 'hidden',
-                numberOfNeighbourMines: 0,
-            }))
-        )
+  constructor(
+    public readonly rows: number,
+    public readonly cols: number,
+    public readonly mines: number,
+  ) {
+    this.cells = Array.from({ length: rows }, () =>
+      Array.from({ length: cols }, () => ({
+        isMine: false,
+        state: "hidden",
+        numberOfNeighbourMines: 0,
+      })),
+    );
 
-        this.placeMines()
-        this.calculateNeighborMines()
-    }
+    this.placeMines();
+    this.calculateNeighborMines();
+  }
 
-    public clickCell(row: number, col: number): Minesweeper {
-        return produce(this, (draft) => {
-            const cell = draft.cells[row][col]
-            if (cell.isMine) {
-                draft.gamestate = 'lost'
-                return
+  public clickCell(row: number, col: number): Minesweeper {
+    return produce(this, (draft) => {
+      const cell = draft.cells[row][col];
+      if (cell.isMine) {
+        draft.gamestate = "lost";
+        return;
+      }
+
+      const cellsToReveal: Coordinate[] = [{ row, col }];
+
+      while (cellsToReveal.length > 0) {
+        const { row, col } = cellsToReveal.shift()!;
+        draft.cells[row][col].state = "revealed";
+        if (draft.cells[row][col].numberOfNeighbourMines === 0) {
+          const neighors = this.getInBoundNeighbourCoordinates(row, col);
+          for (const neighbor of neighors) {
+            if (draft.cells[neighbor.row][neighbor.col].state === "hidden") {
+              cellsToReveal.push(neighbor);
             }
-
-            const cellsToReveal: Coordinate[] = [{ row, col }]
-
-            while (cellsToReveal.length > 0) {
-                const { row, col } = cellsToReveal.shift()!
-                draft.cells[row][col].state = 'revealed'
-                if (draft.cells[row][col].numberOfNeighbourMines === 0) {
-                    const neighors = this.getInBoundNeighbourCoordinates(
-                        row,
-                        col
-                    )
-                    for (const neighbor of neighors) {
-                        if (
-                            draft.cells[neighbor.row][neighbor.col].state ===
-                            'hidden'
-                        ) {
-                            cellsToReveal.push(neighbor)
-                        }
-                    }
-                }
-            }
-        })
-    }
-
-    public toggleFlagged(row: number, col: number): Minesweeper {
-        return produce(this, (draft) => {
-            if (!this.inBounds(row, col)) {
-                return
-            }
-            const cell = draft.cells[row][col]
-            if (cell.state === 'hidden') {
-                cell.state = 'flagged'
-            } else if (cell.state === 'flagged') {
-                cell.state = 'hidden'
-            }
-        })
-    }
-
-    // only call from constructor, edits in place
-    private placeMines(): void {
-        let minesLeftToPlace = this.mines
-        while (minesLeftToPlace > 0) {
-            const row = Math.floor(Math.random() * this.rows)
-            const col = Math.floor(Math.random() * this.cols)
-            if (!this.cells[row][col].isMine) {
-                this.cells[row][col].isMine = true
-                minesLeftToPlace--
-            }
+          }
         }
-    }
+      }
+    });
+  }
 
-    // only call from constructor, edits in place
-    private calculateNeighborMines(): void {
-        for (let row = 0; row < this.rows; row++) {
-            for (let col = 0; col < this.cols; col++) {
-                for (const {
-                    row: neighborRow,
-                    col: neighborCol,
-                } of this.getInBoundNeighbourCoordinates(row, col)) {
-                    if (this.cells[neighborRow][neighborCol].isMine) {
-                        this.cells[row][col].numberOfNeighbourMines++
-                    }
-                }
-            }
+  public toggleFlagged(row: number, col: number): Minesweeper {
+    return produce(this, (draft) => {
+      if (!this.inBounds(row, col)) {
+        return;
+      }
+      const cell = draft.cells[row][col];
+      if (cell.state === "hidden") {
+        cell.state = "flagged";
+      } else if (cell.state === "flagged") {
+        cell.state = "hidden";
+      }
+    });
+  }
+
+  // only call from constructor, edits in place
+  private placeMines(): void {
+    let minesLeftToPlace = this.mines;
+    while (minesLeftToPlace > 0) {
+      const row = Math.floor(Math.random() * this.rows);
+      const col = Math.floor(Math.random() * this.cols);
+      if (!this.cells[row][col].isMine) {
+        this.cells[row][col].isMine = true;
+        minesLeftToPlace--;
+      }
+    }
+  }
+
+  // only call from constructor, edits in place
+  private calculateNeighborMines(): void {
+    for (let row = 0; row < this.rows; row++) {
+      for (let col = 0; col < this.cols; col++) {
+        for (const {
+          row: neighborRow,
+          col: neighborCol,
+        } of this.getInBoundNeighbourCoordinates(row, col)) {
+          if (this.cells[neighborRow][neighborCol].isMine) {
+            this.cells[row][col].numberOfNeighbourMines++;
+          }
         }
+      }
     }
+  }
 
-    private inBounds(row: number, col: number): boolean {
-        return row >= 0 && row < this.rows && col >= 0 && col < this.cols
-    }
+  private inBounds(row: number, col: number): boolean {
+    return row >= 0 && row < this.rows && col >= 0 && col < this.cols;
+  }
 
-    private *getInBoundNeighbourCoordinates(
-        row: number,
-        col: number
-    ): Generator<Coordinate> {
-        const offsets = [-1, 0, 1]
+  private *getInBoundNeighbourCoordinates(
+    row: number,
+    col: number,
+  ): Generator<Coordinate> {
+    const offsets = [-1, 0, 1];
 
-        for (const rowOffset of offsets) {
-            for (const colOffset of offsets) {
-                if (rowOffset === 0 && colOffset === 0) {
-                    continue
-                }
-
-                const newRow = row + rowOffset
-                const newCol = col + colOffset
-                if (this.inBounds(newRow, newCol)) {
-                    yield { row: newRow, col: newCol }
-                }
-            }
+    for (const rowOffset of offsets) {
+      for (const colOffset of offsets) {
+        if (rowOffset === 0 && colOffset === 0) {
+          continue;
         }
-    }
 
-    public indexToCoordinate(index: number): Coordinate {
-        return {
-            row: Math.floor(index / this.cols),
-            col: index % this.cols,
+        const newRow = row + rowOffset;
+        const newCol = col + colOffset;
+        if (this.inBounds(newRow, newCol)) {
+          yield { row: newRow, col: newCol };
         }
+      }
     }
+  }
+
+  public indexToCoordinate(index: number): Coordinate {
+    return {
+      row: Math.floor(index / this.cols),
+      col: index % this.cols,
+    };
+  }
 }
