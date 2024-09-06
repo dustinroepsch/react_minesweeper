ea4fccba34abf0bd000d5b197df4277c7ff59e7a
Dustin Ryan-Roepsch <duryanro@microsoft.com>
2024-09-06 11:14:07 -0700

    center and make the board bigger

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,32 @@ import { useState } from "react";
 import Minesweeper from "./minesweeper";
 import Board from "./components/Board";
 import { useNormalize } from "./normalize";
+import { makeStyles } from "@griffel/react";
 
 function App() {
   useNormalize();
+
   const [minesweeper, setMinesweeper] = useState(
     () => new Minesweeper(10, 10, 10)
   );
 
-  return <Board minesweeper={minesweeper} setMinesweeper={setMinesweeper} />;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Board minesweeper={minesweeper} setMinesweeper={setMinesweeper} />
+    </div>
+  );
 }
+
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+  },
+});
 
 export default App;
